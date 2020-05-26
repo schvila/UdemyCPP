8e9321be617818c68cf5ab0c770b44f85dc935a4
Stanislav Chvila <schvila@gmail.com>
2020-05-26 09:13:21 +0200

    Implements my IPrintable interface to std::ostream

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -4,7 +4,17 @@
 #include <iostream>
 #include<vector>
 
-class Account {
+class IPrintable {
+    friend std::ostream& operator<<(std::ostream& os, const IPrintable &obj);
+public:
+    virtual void print(std::ostream& os) const = 0;
+};
+std::ostream& operator<<(std::ostream& os, const IPrintable &obj) {
+    obj.print(os);
+    return os;
+}
+
+class Account: public IPrintable {
 public:
     virtual void withdraw(double amount) {
         std::cout << "In Account::withdraw" << std::endl;
@@ -13,6 +23,10 @@ public:
     virtual void say_hello() const {
         std::cout << "hello from Account\n";
     }
+    virtual void print(std::ostream& os) const override {
+        os << "Account display";
+    }
+
     /*
     If a class has virtual functions
     ALWAYS provide a public virtual desctructror
@@ -27,6 +41,9 @@ public:
     virtual void say_hello()  { // no const here, no overriding
         std::cout << "hello from Checking\n";
     }
+    virtual void print(std::ostream& os) const override {
+        os << "Checking display";
+    }
 };
 class Savings : public Account {
 public:
@@ -36,12 +53,19 @@ public:
     virtual void say_hello() const override { // now should override
         std::cout << "hello from Savings\n";
     }
+    virtual void print(std::ostream& os) const override {
+        os << "Savings display";
+    }
 };
 class Trust : public Account {
 public:
     virtual void withdraw(double amount) {
         std::cout << "In Trust::withdraw" << std::endl;
     }
+    virtual void print(std::ostream& os) const override {
+        os << "Trust display";
+    }
+
 };
 void do_withdraw(Account& account, double amount) {
     account.withdraw(35);
@@ -61,6 +85,9 @@ int main()
     Account* p2 = new Checking();
     Account* p3 = new Savings();
     Account* p4 = new Trust();
+    std::cout << "\n======== IPrintable ===========\n";
+
+    std::cout << *p1 << " " << *p2 << std::endl;
 
     
     std::cout << "\n======== Array ===========\n";
